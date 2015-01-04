b8f1aeeebc5fe0ff9932c1258a48df79cb8178fe
blreams <blreams@fe13d60a-ce14-447b-9320-7ea61ac4c274>
2015-01-04 19:50:33 +0000

    Modified settings.py to use mysql.

--- collegefootballpick10/settings.py
+++ collegefootballpick10/settings.py
@@ -59,8 +59,14 @@ WSGI_APPLICATION = 'collegefootballpick10.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'collegefootballpick10',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'user_django',
+        'PASSWORD': 'pass_django',
     }
 }
 
