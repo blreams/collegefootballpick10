4af5193a971ceee5ab982ee0289164457b327cb1
Byron Reams <blreams@gmail.com>
2015-05-16 09:17:22 -0400

    Added a UserProfile model, but not sure it works...also migrated which fixed issue with admin registration profiles

--- collegefootballpick10/urls.py
+++ collegefootballpick10/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^admin/password_reset/$', auth_views.password_reset, name='admin_password_reset'),
+    url(r'^admin/password_reset/done/$', auth_views.password_reset_done, name='admin_password_reset_done'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
     url(r'^$', 'pick10.views.index', name='index'),
     url(r'^pick10/', include('pick10.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
--- pick10/models.py
+++ pick10/models.py
@@ -6,6 +6,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    company = models.CharField(max_length=50, blank=True)
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+
 class Conference(models.Model):
     conf_name = models.CharField(max_length=40)                            # Conference name, 'Southeastern'
     div_name = models.CharField(max_length=40, null=True, blank=True)      # Division name, 'East'
