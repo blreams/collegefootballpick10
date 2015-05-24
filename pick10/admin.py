0ded9360cabb765bfcafa6d3c86d04abd2b754bf
Byron Reams <blreams@gmail.com>
2015-05-24 13:49:58 -0400

    Minor mods to pick10/admin.py

--- pick10/admin.py
+++ pick10/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
-from pick10.models import Conference, Team, Game, Week, Pick
+from .models import Conference, Team, Game, Week, Pick
 
 class ConferenceAdmin(admin.ModelAdmin):
-    list_display = ('conf_name', 'div_name')
+    list_display = ('conf_name', 'div_name', 'created', 'updated')
 
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('team_name', 'mascot')
+    list_display = ('team_name', 'mascot', 'created', 'updated')
 
 class GameAdmin(admin.ModelAdmin):
-    list_display = ('game_year', 'game_week', 'game_num', 'team1', 'team2')
+    list_display = ('game_year', 'game_week', 'game_num', 'team1', 'team2', 'created', 'updated')
 
 class WeekAdmin(admin.ModelAdmin):
-    list_display = ('week_year', 'week_num', 'lock_picks', 'lock_scores')
+    list_display = ('week_year', 'week_num', 'lock_picks', 'lock_scores', 'created', 'updated')
 
 class PickAdmin(admin.ModelAdmin):
-    list_display = ('pick_week', 'pick_user', 'pick_game')
+    list_display = ('pick_week', 'pick_user', 'pick_game', 'created', 'updated')
 
 admin.site.register(Conference, ConferenceAdmin)
 admin.site.register(Team, TeamAdmin)
