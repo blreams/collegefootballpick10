1bf41a525689735e3babc6df346a942f5c366e63
Brent Holden <brent.l.holden@gmail.com>
2015-06-30 15:42:07 -0400

    setup database test skeleton

---
+++ pick10/tests/test_database.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+# This class tests the database.py file
+class DatabaseTest(TestCase):
+
+    def test_load_week_data(self):
+        self.fail('Not Implemented yet')
