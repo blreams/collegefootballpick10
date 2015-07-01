e42a1539c8507d61c176be177a4c7a5df021f27b
Brent Holden <brent.l.holden@gmail.com>
2015-07-01 16:26:57 -0400

    improved unit test database code

--- pick10/tests/unit_test_database.py
+++ pick10/tests/unit_test_database.py
@@ -2,21 +2,29 @@
 # The class creates a central location for setting up a database 
 # so each unit test doesn't have to reinvent the wheel every time.
 from pick10.models import *
+from stage_history import populate_conferences_teams
 
 class UnitTestDatabase:
 
+    def __init__(self):
+        populate_conferences_teams()
+
     def setup_simple_week(self,year=2014,week_number=1):
         week = add_week(year,week_number)
-        conf = add_conference('ACC')
-        team1 = add_team('Georgia Tech','Buzz',conf)
-        team2 = add_team('Clemson','Tigers',conf)
-        add_game(week,team1,team2,game_num=1,favored=1,spread=0.5)
-        add_game(week,team1,team2,game_num=2,favored=1,spread=0.5)
-        add_game(week,team1,team2,game_num=3,favored=1,spread=0.5)
-        add_game(week,team1,team2,game_num=4,favored=1,spread=0.5)
-        add_game(week,team1,team2,game_num=5,favored=1,spread=0.5)
-        add_game(week,team1,team2,game_num=6,favored=1,spread=0.5)
-        add_game(week,team1,team2,game_num=7,favored=1,spread=0.5)
-        add_game(week,team1,team2,game_num=8,favored=1,spread=0.5)
-        add_game(week,team1,team2,game_num=9,favored=1,spread=0.5)
-        add_game(week,team1,team2,game_num=10,favored=1,spread=0.5)
+        self.setup_game(week,1,"Georgia Tech","Clemson",favored=1,spread=0.5)
+        self.setup_game(week,2,"Duke","North Carolina",favored=2,spread=1.5)
+        self.setup_game(week,3,"Virginia","Virginia Tech",favored=1,spread=7.5)
+        self.setup_game(week,4,"Indiana","Maryland",favored=2,spread=3.5)
+        self.setup_game(week,5,"South Carolina","Georgia",favored=1,spread=9.5)
+        self.setup_game(week,6,"Tennessee","Vanderbilt",favored=2,spread=11.5)
+        self.setup_game(week,7,"Auburn","Alabama",favored=1,spread=1.5)
+        self.setup_game(week,8,"Southern Cal","UCLA",favored=2,spread=4.5)
+        self.setup_game(week,9,"Army","Navy",favored=2,spread=5.5)
+        self.setup_game(week,10,"Notre Dame","Florida State",favored=1,spread=0.5)
+
+    def setup_game(self,week,game_number,team1_name,team2_name,favored,spread):
+        team1 = get_team(team1_name)
+        team2 = get_team(team2_name)
+        add_game(week,team1,team2,game_num=game_number,favored=favored,spread=spread)
+
+
