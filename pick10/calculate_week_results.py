6c1e28bad4d15b53ba1e6b1878e7d73aebcec1af
Brent Holden <brent.l.holden@gmail.com>
2015-07-18 15:23:13 -0400

    added calculate_week_results file with unimplemented functions

---
+++ pick10/calculate_week_results.py
@@ -0,0 +1,14 @@
+from calculator import *
+from database import *
+
+class CalculateWeekResults:
+
+    def __init__(self,year,week_number):
+        self.year = year
+        self.week_number = week_number
+
+    def get_results(self):
+        pass
+
+    def __calculate_week_results(self):
+        pass
