64320efdd22d20727eb116ab77f3742729e49a9b
Brent Holden <brent.l.holden@gmail.com>
2015-07-26 16:09:18 -0400

    generic week results page displayed

--- pick10/week_results_view.py
+++ pick10/week_results_view.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from pick10.models import *
 from pick10.calculate_week_results import *
 
@@ -31,8 +32,7 @@ class WeekResultsView:
         params['year'] = year
         params['week_number'] = week_number
         #params['weeks_in_year'] = weeks_in_year
-        #params['content'] = self.__initial_content(results,winner_info)
-        params['content'] = '<div>Week Results Content</div>'
+        params['content'] = self.__initial_content(results,winner_info)
         #params['sorted_by_wins'] = self.__sort_by_wins(results,winner_info)
         #params['sorted_by_wins_reversed'] = self.__sort_by_wins_reversed(results,winner_info)
         #params['sorted_by_losses'] = self.__sort_by_losses(results,winner_info)
@@ -51,6 +51,40 @@ class WeekResultsView:
 
         return render(request,"pick10/week_results.html",params)
 
+    def __initial_content(self,results,winner_info):
+        return self.__sort_by_wins(results,winner_info,escape=False)
+
+    def __sort_by_wins(self,results,winner_info,escape=True):
+        sorted_by_players = sorted(results,key=lambda result:result.player_name)
+        sorted_by_wins = sorted(sorted_by_players,key=lambda result:result.rank)
+        self.__move_winner_to_top(sorted_by_wins,results)
+        highlight = self.__highlight_column('wins')
+
+        params = dict()
+        params['results'] = sorted_by_wins
+        params['winner'] = winner_info
+        params.update(highlight)
+
+        content = render_to_string(self.__render_file,params)
+        import pdb; pdb.set_trace()
+        if escape:
+            html_str = self.escape_string(content)
+            return self.compress_html(html_str)
+        return content
+
+    def __move_winner_to_top(self,sorted_data,results,use_projected_rank=False):
+        for result in results:
+            if use_projected_rank and result.projected_rank != 1:
+                continue
+
+            if use_projected_rank == False and result.rank != 1:
+                continue
+
+            if result.winner != None and result.winner != "":
+                index = self.__find_player_id(result.player_id,sorted_data)
+                item = sorted_data.pop(index)
+                sorted_data.insert(0,item)
+
     def __bad_year_or_week_number(self,year,week_number):
         try:
             year_int = int(year)
@@ -59,3 +93,41 @@ class WeekResultsView:
         except Exception:
             return True
         return False
+
+    def __highlight_column(self,name):
+        d = dict()
+
+        if name == "wins":
+            d['wins_id'] = "highlight-content"
+        else:
+            d['wins_id'] = "content"
+
+        if name == "losses":
+            d['losses_id'] = "highlight-content"
+        else:
+            d['losses_id'] = "content"
+
+        if name == "projected_wins":
+            d['projected_id'] = "highlight-content"
+        else:
+            d['projected_id'] = "content"
+
+        if name == "possible_wins":
+            d['possible_id'] = "highlight-content"
+        else:
+            d['possible_id'] = "content"
+
+        return d
+
+    def __find_player_id(self,player_id,data):
+        for i,item in enumerate(data):
+            if player_id == item.player_id:
+                return i
+        raise AssertionError
+
+    def escape_string(self,s):
+        return string.replace(s,'"','\\\"')
+
+    def compress_html(self,html):
+        s1 = string.replace(html,'\n','')
+        return re.sub(r'\s\s+',' ',s1)
