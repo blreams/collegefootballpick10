29597284dec0b7b43e6cc5359332eee40cd7f2e4
Brent Holden <brent.l.holden@gmail.com>
2015-07-27 16:14:18 -0400

    copied over player_result function outline from google app

---
+++ pick10/calculate_player_results.py
@@ -0,0 +1,19 @@
+class CalculatePlayerResults:
+
+    def __init__(self,year,week_number,player_id,private_names=False):
+        pass
+
+    def get_results(self):
+        return self.__results
+
+    def __calculate_player_results(self,player_id,year,week_number): 
+        pass
+
+    def __calculate_player_summary(self,player_id,calc,week_data):
+        pass
+
+    def __calculate_player_game_results_sorted_by_game_number(self,player_key,calc,week_data):
+        pass
+
+    def __calculate_player_game_result(self,player_key,game_key,game,calc,week_data):
+        pass
