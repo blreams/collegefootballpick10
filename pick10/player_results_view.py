63d39fa0a97cbf51e63a5f698dbc857d05018e47
Brent Holden <brent.l.holden@gmail.com>
2015-07-28 15:48:12 -0400

    setup player results bad id template and css

--- pick10/player_results_view.py
+++ pick10/player_results_view.py
@@ -7,6 +7,46 @@ import re
 
 class PlayerResultsView:
 
-    def get(self,request,year,week_number):
-        return None
+    def get(self,request,year,week_number,player_id):
 
+        if self.__bad_year_or_week_number(year,week_number):
+            data={'year':year,'week_number':week_number}
+            return render(request,"pick10/bad_week.html",data)
+
+        if not(self.__is_player_id_valid(player_id)):
+            data={'year':year,'player_id':player_id,'error':'bad_id'}
+            render("bad_player.html",data)
+            return
+
+        year = int(year)
+        week_number = int(week_number)
+        player_id = int(player_id)
+
+        use_private_names = request.user.is_authenticated()
+
+        if not(self.__is_player_in_year(player_id,year)):
+            data={'year':year,'player_id':player_id,'error':'bad_year'}
+            render("bad_player.html",data)
+            return
+
+    def __bad_year_or_week_number(self,year,week_number):
+        try:
+            year_int = int(year)
+            week_int = int(week_number)
+            w = get_week(year_int,week_int)
+        except Exception:
+            return True
+        return False
+
+    def __is_player_id_valid(self,player_id):
+        try:
+            id_int = int(player_id)
+            player = Player.objects.get(id=id_int)
+        except Exception:
+            return False
+        return True
+
+    def __is_player_in_year(self,player_id,year):
+        d = Database()
+        players_in_year = d.load_players(year)
+        return player_id in players_in_year
