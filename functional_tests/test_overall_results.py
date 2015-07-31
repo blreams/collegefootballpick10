96ab999328d96ac5c24b9f10c66f457b89f44f2b
Brent Holden <brent.l.holden@gmail.com>
2015-07-31 14:08:23 -0400

    overall results code and templates

--- functional_tests/test_overall_results.py
+++ functional_tests/test_overall_results.py
@@ -7,3 +7,12 @@ class OverallResultsTest(FunctionalTest):
 
     def test_page_up(self):
         pass
+
+    def test_bad_year(self):
+        self.__open_results_page(year=1980)
+        body = self.browser.find_element_by_tag_name('body').text
+        self.assertIn('Cannot find a pool for the year 1980',body)
+
+    def __open_results_page(self,year):
+        address = self.server_url + reverse('overall_results',args=(year,))
+        self.browser.get(address)
