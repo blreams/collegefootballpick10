447cc74007d9cc470d5bf75b50d7fdfb8b1498ca
Brent Holden <brent.l.holden@gmail.com>
2015-08-08 14:29:23 -0400

    Implemented HttpResponseNotFound for invalid request method types

--- pick10/views.py
+++ pick10/views.py
@@ -7,6 +7,7 @@ from update_view import *
 from tiebreak_view import *
 from update_games_view import *
 from django.core.cache import *
+from django.http import HttpResponseNotFound
 
 @login_required
 def home(request):
@@ -16,40 +17,58 @@ def index(request):
     return render(request, 'pick10/index.html')
 
 def overall_results(request,year):
-    response = OverallResultsView().get(year)
-    return response
+    if request.method == "GET":
+        response = OverallResultsView().get(year)
+        return response
+    return HttpResponseNotFound('<h1>Page not found</h1>')
 
 def week_results(request,year,week_number):
-    response = WeekResultsView().get(year,week_number)
-    return response
+    if request.method == "GET":
+        response = WeekResultsView().get(year,week_number)
+        return response
+    return HttpResponseNotFound('<h1>Page not found</h1>')
 
 def player_results(request,year,week_number,player_id):
-    response = PlayerResultsView().get(request,year,week_number,player_id)
-    return response
+    if request.method == "GET":
+        response = PlayerResultsView().get(request,year,week_number,player_id)
+        return response
+    return HttpResponseNotFound('<h1>Page not found</h1>')
 
 def tiebreak(request,year,week_number):
-    response = TiebreakView().get(year,week_number)
-    return response
+    if request.method == "GET":
+        response = TiebreakView().get(year,week_number)
+        return response
+    return HttpResponseNotFound('<h1>Page not found</h1>')
 
 def update_games(request,year,week_number):
     if request.method == "GET":
         response = UpdateGamesView().get(request,year,week_number)
+        return response
     elif request.method == "POST":
         response = UpdateGamesView().post(request,year,week_number)
-    return response
+        return response
+    return HttpResponseNotFound('<h1>Page not found</h1>')
 
 def update_pages(request,year,week_number):
-    response = UpdatePageView().get(year,week_number)
-    return response
+    if request.method == "GET":
+        response = UpdatePageView().get(year,week_number)
+        return response
+    return HttpResponseNotFound('<h1>Page not found</h1>')
 
 def update_overall(request,year):
-    response = UpdatePageView().update_overall(year)
-    return response
+    if request.method == "POST":
+        response = UpdatePageView().update_overall(year)
+        return response
+    return HttpResponseNotFound('<h1>Page not found</h1>')
 
 def update_week(request,year,week_number):
-    response = UpdatePageView().update_week(year,week_number)
-    return response
+    if request.method == "POST":
+        response = UpdatePageView().update_week(year,week_number)
+        return response
+    return HttpResponseNotFound('<h1>Page not found</h1>')
 
 def update_tiebreak(request,year,week_number):
-    response = UpdatePageView().update_tiebreak(year,week_number)
-    return response
+    if request.method == "POST":
+        response = UpdatePageView().update_tiebreak(year,week_number)
+        return response
+    return HttpResponseNotFound('<h1>Page not found</h1>')
