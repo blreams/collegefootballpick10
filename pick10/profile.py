f24a73f308825a9d08ebef12cc4d409fe5639e2a
Byron Reams <blreams@gmail.com>
2015-08-13 19:30:27 -0400

    UserProfile form uses bootstrap now.

--- pick10/profile.py
+++ pick10/profile.py
@@ -12,19 +12,21 @@ class ProfileView:
 
         userprofile, created = UserProfile.objects.get_or_create(user=user)
         form = UserProfileForm(instance=userprofile)
-        context = {'form': form}
+        choices = [choice[0] for choice in form.fields['preferredtz'].choices]
+        context = {'form': form, 'choices': choices, 'userprofile': userprofile}
         return render(request,"pick10/profile_form.html", context)
 
     def post(self,request):
         user = request.user
+        userprofile = UserProfile.objects.get(user=user)
         form = UserProfileForm(request.POST)
-        context = {'form': form}
+        choices = [choice[0] for choice in form.fields['preferredtz'].choices]
+        context = {'form': form, 'choices': choices, 'userprofile': userprofile}
         if form.is_valid():
             cd = form.cleaned_data
-            userprofile = UserProfile.objects.get(user=user)
             userprofile.company = cd['company']
             userprofile.preferredtz = cd['preferredtz']
             userprofile.save()
             return redirect('/pick10/')
-        return render(request, 'pick10/user_profile.html', context)
+        return render(request, 'pick10/profile_form.html', context)
 
