c9aa56dd75c9ab4bbe14e94d2a2d838632ec1170
Byron Reams <blreams@gmail.com>
2015-08-16 00:25:01 -0400

    Spent some time cleaning up base.html to get the right functionality and proper links in the navbar.

--- collegefootballpick10/urls.py
+++ collegefootballpick10/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
-    url(r'^$', 'pick10.views.index', name='index'),
+    url(r'^$', 'pick10.views.home', name='home'),
     url(r'^pick10/', include('pick10.urls')),
     url(r'^accounts/', include('registration.backends.default.urls')),
 )
