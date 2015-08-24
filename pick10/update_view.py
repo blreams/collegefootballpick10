bf5fec9b6be934fd463c707cbf6ad3a132c38bd3
Brent Holden <brent.l.holden@gmail.com>
2015-08-23 21:57:57 -0400

    update page checks for public/private/superuser

--- pick10/update_view.py
+++ pick10/update_view.py
@@ -3,24 +3,46 @@ from django.template.loader import render_to_string
 from pick10.overall_results_view import *
 from pick10.week_results_view import *
 from pick10.tiebreak_view import *
+from pick10.user_access import *
+from django.http import HttpResponseNotFound
 
 class UpdatePageView:
 
     def get(self,request,year,week_number):
-        data={'year':year,'week_number':week_number}
-        return render(request, "pick10/update_pages.html", data)
+        access = UserAccess(request.user)
+
+        if access.is_superuser() or access.is_private_user():
+            data={'year':year,'week_number':week_number}
+            return render(request, "pick10/update_pages.html", data)
+
+        return HttpResponseNotFound('<h1>Page not found</h1>')
 
     def update_overall(self,request,year):
-        response = OverallResultsView().get(request,year,use_private_names=False,use_memcache=False)
-        response = OverallResultsView().get(request,year,use_private_names=True,use_memcache=False)
-        return HttpResponse("<html>success</html>")
+        access = UserAccess(request.user)
+
+        if access.is_superuser() or access.is_private_user():
+            response = OverallResultsView().get(request,year,use_private_names=False,use_memcache=False)
+            response = OverallResultsView().get(request,year,use_private_names=True,use_memcache=False)
+            return HttpResponse("<html>success</html>")
+
+        return HttpResponseNotFound('<h1>Page not found</h1>')
 
     def update_week(self,request,year,week_number):
-        response = WeekResultsView().get(request,year,week_number,use_private_names=False,use_memcache=False)
-        response = WeekResultsView().get(request,year,week_number,use_private_names=True,use_memcache=False)
-        return HttpResponse("<html>success</html>")
+        access = UserAccess(request.user)
+
+        if access.is_superuser() or access.is_private_user():
+            response = WeekResultsView().get(request,year,week_number,use_private_names=False,use_memcache=False)
+            response = WeekResultsView().get(request,year,week_number,use_private_names=True,use_memcache=False)
+            return HttpResponse("<html>success</html>")
+
+        return HttpResponseNotFound('<h1>Page not found</h1>')
 
     def update_tiebreak(self,request,year,week_number):
-        response = TiebreakView().get(request,year,week_number,use_private_names=False,use_memcache=False)
-        response = TiebreakView().get(request,year,week_number,use_private_names=True,use_memcache=False)
-        return HttpResponse("<html>success</html>")
+        access = UserAccess(request.user)
+
+        if access.is_superuser() or access.is_private_user():
+            response = TiebreakView().get(request,year,week_number,use_private_names=False,use_memcache=False)
+            response = TiebreakView().get(request,year,week_number,use_private_names=True,use_memcache=False)
+            return HttpResponse("<html>success</html>")
+
+        return HttpResponseNotFound('<h1>Page not found</h1>')
