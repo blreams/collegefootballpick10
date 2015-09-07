65b6274ce74d5e5b5dfcc7e86611db2569fd9ddc
Brent Holden <brent.l.holden@gmail.com>
2015-09-06 23:35:01 -0400

    no picks tests for week final

--- functional_tests/test_no_picks.py
+++ functional_tests/test_no_picks.py
@@ -271,11 +271,162 @@ class NoPicksTest(FunctionalTest):
         test_db.delete_database()
 
 
+    @unittest.skip('tested')
     def test_week_results_week2_in_progress_player2(self):
         # login player with no picks
         test_db = UnitTestDatabase()
         test_db.setup_week_final(1978,1)
         test_db.setup_week_in_progress_with_no_pick_defaulters(1978,2)
+        self.__login_player('John',1978)
+
+        player_with_picks = self.utils.get_player_from_public_name(1978,'Brent')
+        player_no_picks = self.utils.get_player_from_public_name(1978,'John')
+
+        self.utils.week_results_page(1978,1)
+        self.__is_page_up('Week 1 Leaderboard')
+
+        self.utils.week_results_page(1978,2)
+        self.__is_page_up('Week 2 Leaderboard')
+
+        self.utils.overall_results_page(1978)
+        self.__is_page_up('1978 Leaderboard')
+
+        self.utils.player_results_page(1978,1,player_with_picks.id)
+        self.__is_page_up('Brent Week 1 Results')
+
+        self.utils.player_results_page(1978,2,player_with_picks.id)
+        self.__is_page_up('Brent Week 2 Results')
+
+        self.utils.player_results_page(1978,1,player_no_picks.id)
+        self.__is_page_up('John Week 1 Results')
+
+        self.utils.player_results_page(1978,2,player_no_picks.id)
+        self.__is_page_up('John Week 2 Results')
+
+        self.utils.enter_picks_page(1978,1,player_no_picks.id)
+        self.__is_page_up('The week is final')
+
+        self.utils.enter_picks_page(1978,2,player_no_picks.id)
+        self.__is_page_up('The week is currently in progress')
+
+        self.utils.update_games_page(1978,1)
+        self.__is_page_up('Week 1 Games')
+
+        self.utils.update_games_page(1978,2)
+        self.__is_page_up('Week 2 Games')
+
+        test_db.delete_database()
+
+    @unittest.skip('tested')
+    def test_week_results_week1_final(self):
+        test_db = UnitTestDatabase()
+        test_db.setup_week_final_with_no_pick_defaulters(1978,1)
+        self.__login_player('Brent',1978)
+
+        player_with_picks = self.utils.get_player_from_public_name(1978,'Brent')
+        player_no_picks = self.utils.get_player_from_public_name(1978,'John')
+
+        self.utils.week_results_page(1978,1)
+        self.__is_page_up('Week 1 Leaderboard')
+
+        self.utils.overall_results_page(1978)
+        self.__is_page_up('1978 Leaderboard')
+
+        self.utils.player_results_page(1978,1,player_with_picks.id)
+        self.__is_page_up('Brent Week 1 Results')
+
+        self.utils.player_results_page(1978,1,player_no_picks.id)
+        self.__is_page_up('John Week 1 Results')
+
+        self.utils.enter_picks_page(1978,1,player_with_picks.id)
+        self.__is_page_up('The week is final')
+
+        self.utils.update_games_page(1978,1)
+        self.__is_page_up('Week 1 Games')
+
+        test_db.delete_database()
+
+    @unittest.skip('tested')
+    def test_week_results_week2_final(self):
+        test_db = UnitTestDatabase()
+        test_db.setup_week_final(1978,1)
+        test_db.setup_week_final_with_no_pick_defaulters(1978,2)
+        self.__login_player('Brent',1978)
+
+        player_with_picks = self.utils.get_player_from_public_name(1978,'Brent')
+        player_no_picks = self.utils.get_player_from_public_name(1978,'John')
+
+        self.utils.week_results_page(1978,1)
+        self.__is_page_up('Week 1 Leaderboard')
+
+        self.utils.week_results_page(1978,2)
+        self.__is_page_up('Week 2 Leaderboard')
+
+        self.utils.overall_results_page(1978)
+        self.__is_page_up('1978 Leaderboard')
+
+        self.utils.player_results_page(1978,1,player_with_picks.id)
+        self.__is_page_up('Brent Week 1 Results')
+
+        self.utils.player_results_page(1978,2,player_with_picks.id)
+        self.__is_page_up('Brent Week 2 Results')
+
+        self.utils.player_results_page(1978,1,player_no_picks.id)
+        self.__is_page_up('John Week 1 Results')
+
+        self.utils.player_results_page(1978,2,player_no_picks.id)
+        self.__is_page_up('John Week 2 Results')
+
+        self.utils.enter_picks_page(1978,1,player_with_picks.id)
+        self.__is_page_up('The week is final')
+
+        self.utils.enter_picks_page(1978,2,player_with_picks.id)
+        self.__is_page_up('The week is final')
+
+        self.utils.update_games_page(1978,1)
+        self.__is_page_up('Week 1 Games')
+
+        self.utils.update_games_page(1978,2)
+        self.__is_page_up('Week 2 Games')
+
+        test_db.delete_database()
+
+    @unittest.skip('tested')
+    def test_week_results_week1_final_player2(self):
+        # login player with no picks
+        test_db = UnitTestDatabase()
+        test_db.setup_week_final_with_no_pick_defaulters(1978,1)
+        self.__login_player('John',1978)
+
+        player_with_picks = self.utils.get_player_from_public_name(1978,'Brent')
+        player_no_picks = self.utils.get_player_from_public_name(1978,'John')
+
+        self.utils.week_results_page(1978,1)
+        self.__is_page_up('Week 1 Leaderboard')
+
+        self.utils.overall_results_page(1978)
+        self.__is_page_up('1978 Leaderboard')
+
+        self.utils.player_results_page(1978,1,player_with_picks.id)
+        self.__is_page_up('Brent Week 1 Results')
+
+        self.utils.player_results_page(1978,1,player_no_picks.id)
+        self.__is_page_up('John Week 1 Results')
+
+        self.utils.enter_picks_page(1978,1,player_no_picks.id)
+        self.__is_page_up('The week is final')
+
+        self.utils.update_games_page(1978,1)
+        self.__is_page_up('Week 1 Games')
+
+        test_db.delete_database()
+
+
+    def test_week_results_week2_final_player2(self):
+        # login player with no picks
+        test_db = UnitTestDatabase()
+        test_db.setup_week_final(1978,1)
+        test_db.setup_week_final_with_no_pick_defaulters(1978,2)
         self.__login_player('John',1978)
 
         player_with_picks = self.utils.get_player_from_public_name(1978,'Brent')
@@ -307,7 +458,7 @@ class NoPicksTest(FunctionalTest):
         self.__is_page_up('The week is final')
 
         self.utils.enter_picks_page(1978,2,player_no_picks.id)
-        self.__is_page_up('The week is currently in progress')
+        self.__is_page_up('The week is final')
 
         self.utils.update_games_page(1978,1)
         self.__is_page_up('Week 1 Games')
