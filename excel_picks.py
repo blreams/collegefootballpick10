fdec00da79db8f8dafb6a19e2fc3d8182a35349c
Brent Holden <brent.l.holden@gmail.com>
2016-09-18 15:32:11 -0400

    Added game favored, spread, and team score information to the generated week picks excel spreadsheet

--- excel_picks.py
+++ excel_picks.py
@@ -110,17 +110,75 @@ def get_games_column(data):
             column.append("%s - score" % (game.team2.team_name))
     return column
 
+def get_game_row(data,df,game_number,team_number,game10_score_row=False):
+    assert team_number == 1 or team_number == 2,"Unexpected team number"
+    assert game_number in [1,2,3,4,5,6,7,8,9,10],"Unexpected game number"
+
+    game = data.games[game_number]
+    if team_number == 1:
+        team_name = game.team1.team_name
+    elif team_number == 2:
+        team_name = game.team2.team_name
+
+    if game_number == 10 and game10_score_row:
+        row_name = '%s - score' % (team_name)
+    else:
+        row_name = team_name
+
+    assert row_name in df.index,"Could not find game row in dataframe"
+
+    return row_name
+
+
 def create_picks_sheet(year,week_number,data):
+    favorite_column = 'Favorite by:'
+    score_column = 'Actual Score'
+
     player_names = sorted([ p.ss_name for p in data.players.values() ])
     games = get_games_column(data)
-    picks_df = pd.DataFrame(columns=player_names,index=games)
+    columns = [favorite_column,score_column] + player_names
+    picks_df = pd.DataFrame(columns=columns,index=games)
 
+    # add the player picks
     for player_id in data.players:
         player = data.players[player_id]
         column_data = get_picks_column(player_id,data)
         if len(column_data) < 22:
             column_data = ['' for i in range(22)]
         picks_df[player.ss_name] = column_data
+
+    # add the game favored/spread and team scores
+    game_numbers = [ number for number in data.games.keys() ]
+    assert game_numbers == [1,2,3,4,5,6,7,8,9,10], "Games 1 thru 10 not found in data"
+
+    for game_num in range(1,11):
+        game = data.games[game_num]
+
+        team1_row = get_game_row(data,picks_df,game_num,team_number=1)
+        team2_row = get_game_row(data,picks_df,game_num,team_number=2)
+
+        # fill in the favored team
+        if game.favored == 1:
+            picks_df[favorite_column][team1_row] = game.spread
+        elif game.favored == 2:
+            picks_df[favorite_column][team2_row] = game.spread
+        else:
+            print "Favored info not available"
+
+        # fill in the scores if available
+        score_not_entered = game.team1_actual_points < 0 and game.team2_actual_points < 0
+        score_entered = game.team1_actual_points >= 0 and game.team2_actual_points >= 0
+        assert score_not_entered or score_entered, "Team score for game %d in inconsistent state" % (game_num)
+
+        if score_entered:
+            picks_df[score_column][team1_row] = game.team1_actual_points
+            picks_df[score_column][team2_row] = game.team2_actual_points
+
+            if game_num == 10:  # game 10 requires score filled in on 2 rows
+                team1_score_row = get_game_row(data,picks_df,game_num,team_number=1,game10_score_row=True)
+                team2_score_row = get_game_row(data,picks_df,game_num,team_number=2,game10_score_row=True)
+                picks_df[score_column][team1_score_row] = game.team1_actual_points
+                picks_df[score_column][team2_score_row] = game.team2_actual_points
 
     filename = 'picks_%d_%d.xls' % (year,week_number)
     sheet = 'week_%d' % (week_number)
