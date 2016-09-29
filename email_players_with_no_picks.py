4568e8fb2186d16d50d207caf20686c3c745984d
Brent Holden <brent.l.holden@gmail.com>
2016-09-29 13:47:01 -0400

    updated the email picks reminder script

--- email_players_with_no_picks.py
+++ email_players_with_no_picks.py
@@ -1,6 +1,6 @@
 # This script sends an email reminder to players that haven't entered picks.
 
-SCRIPT_TEST = True
+SCRIPT_TEST = False
 
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collegefootballpick10.settings')
@@ -17,7 +17,13 @@ import django.utils.timezone as tz
 import datetime
 import collegefootballpick10.settings as cfp_settings
 
+class PlayerInfo:
+    player = None
+    player_id = None
+    email = None
+    timezone = None
 
+players = []
 database = Database()
 
 def get_current_week():
@@ -36,85 +42,122 @@ def get_current_week():
 
     return current_year,current_week
 
-def trigger_script(data):
+def trigger_script(deadline):
     if SCRIPT_TEST:
         return True
 
     # this script is run once a day at the same time
     # trigger only when script run within 24 hours of the deadline
     current_time_utc = tz.now()
-    if current_time_utc <= data.week.pick_deadline:
-        time_diff = data.week.pick_deadline - current_time_utc
+    if current_time_utc <= deadline:
+        time_diff = deadline - current_time_utc
         return time_diff < datetime.timedelta(1)
 
     return False
 
-def get_players_without_picks(data):
+def find_players_without_picks(data):
     calc = CalculateResults(data)
 
-    players = []
     for player_id in data.players:
         player = data.players[player_id]
 
         if calc.did_player_default(player):
-            players.append(player)
-    return players
+            p = PlayerInfo()
+            p.player = player
+            p.player_id = player_id
+            players.append(p)
 
-def get_email_addresses(players):
-    addresses = []
-    for player in players:
-        profile = get_profile_by_player(player)
-        if profile == None:
-            print "No email address found for %s" % (player.private_name)
+def fill_in_player_email_and_timezone():
+    for i in range(len(players)):
+        profile = get_profile_by_player(players[i].player)
+        if profile == None or profile.user.email == None:
+            print "No email address found for %s" % (players[i].player.private_name)
+            players[i].email = None
+            players[i].timezone = 'US/Eastern'
             continue
-        addresses.append(profile.user.email)
-    return addresses
+        players[i].email = profile.user.email
 
-def deadline_as_string(deadline_utc):
-    tz = pytz.timezone('US/Eastern')
-    deadline_est = deadline_utc.astimezone(tz)
+        if profile.preferredtz == None:
+            players[i].timezone = 'US/Eastern'
+        else:
+            players[i].timezone = profile.preferredtz
+
+def deadline_as_string(deadline_utc,timezone):
+    tz = pytz.timezone(timezone)
+    deadline = deadline_utc.astimezone(tz)
     date_format = "%a %m/%d/%Y %I:%M %p %Z"
-    return deadline_est.strftime(date_format)
+    return deadline.strftime(date_format)
 
-def send_email(year,week,deadline,addresses):
+def get_reminder_email_text(year,week,deadline,player):
     subject = "CollegeFootballPick10 %d Week %d Picks Reminder" % (year,week)
 
-    message = "The CollegeFootballPick10 site has detected that you have not entered any picks for %d week %d.\n" % (year,week) +\
-              "This email is a reminder to enter picks before the deadline at %s.\n" % (deadline_as_string(deadline))
-    message += "\nhttp://collegefootballpick10.pythonanywhere.com/pick10/%d/week/%d/results\n" % (year,week)
+    deadline_str = deadline_as_string(deadline,player.timezone)
 
-    send_mail(subject,message,cfp_settings.DEFAULT_FROM_EMAIL,addresses)
+    message =  "The CollegeFootballPick10 site has detected that you have not entered any picks for %d week %d.\n" % (year,week)
+    message += "This email is a reminder to enter picks before the deadline at %s.\n\n" % (deadline_str)
+    message += "Link to Your Enter Picks Page:\n" 
+    message += "http://collegefootballpick10.pythonanywhere.com/pick10/%d/week/%d/player/%d/picks\n" % (year,week,player.player_id)
 
-def script_test_email(year,week,deadline,addresses):
-    subject = "CollegeFootballPick10 %d Week %d Picks Reminder TEST" % (year,week)
+    return subject,message
 
-    message = "This is a test email for the Picks Reminder Email Script\n\n"
-    message += "The following players have not entered picks yet:\n"
+def send_pick_reminder_email(year,week,deadline,player):
+    subject, message = get_reminder_email_text(year,week,deadline,player)
+    try:
+        send_mail(subject,message,cfp_settings.DEFAULT_FROM_EMAIL,[player.email])
+    except:
+        print "Failed to send an email to %s" % (player.email)
 
-    for addr in email_addresses:
-        message += "%s\n" % (addr)
+def send_admin_email(year,week,deadline):
+    subject = "CollegeFootballPick10 %d Week %d Picks Reminder Notification" % (year,week)
 
-    message += "\nhttp://collegefootballpick10.pythonanywhere.com/pick10/%d/week/%d/results\n" % (year,week)
+    message  = "The script to send a pick reminder email has been run.\n\n"
+
+    if len(players) <= 0:
+        message += "The script detected that all players have entered their picks.\n\n"
+    else:
+        message += "The script detected the following players have not entered picks:\n"
+        for player in players:
+            name = player.player.private_name
+            email = player.email if player.email != None else 'No email address available'
+            message += '%s (%s)\n' % (name,email)
+        message += '\n'
+
+        message += 'The following is an example email that was sent:\n'
+        example_subject, example_message = get_reminder_email_text(year,week,deadline,players[0])
+        message += example_message
+        message += "\n"
+
+    if SCRIPT_TEST:
+        print subject
+        print message
 
     to_field = ["brent.l.holden@gmail.com","blreams@gmail.com"]
 
-    send_mail(subject,message,cfp_settings.DEFAULT_FROM_EMAIL,to_field)
+    try:
+        send_mail(subject,message,cfp_settings.DEFAULT_FROM_EMAIL,to_field)
+    except:
+        print "Failed to send an email to %s" % (to_field)
 
 
-# if an error occurs, just let the script fail with an exception
 if __name__ == "__main__":
+
     year,week = get_current_week()
-    print "Finding players with no picks for %d week %d" % (year,week)
-
     data = database.load_week_data(year,week)
-   
-    if trigger_script(data):
+    deadline = data.week.pick_deadline
 
-        players = get_players_without_picks(data)
-        email_addresses = get_email_addresses(players)
+    if trigger_script(deadline):
 
-        if SCRIPT_TEST:
-            script_test_email(year,week,data.week.pick_deadline,email_addresses)
+        print "Finding players with no picks for %d week %d" % (year,week)
+
+        find_players_without_picks(data)
+        fill_in_player_email_and_timezone()
+
+        send_admin_email(year,week,deadline)
+
+        if len(players) == 0:
+            print "All players have entered their picks."
         else:
-            send_email(year,week,data.week.pick_deadline,email_addresses)
-
+            for player in players:
+                if player.email != None:
+                    print "Sending reminder email to %s" % (player.email)
+                    send_pick_reminder_email(year,week,deadline,player)
