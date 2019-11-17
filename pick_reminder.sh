84ed74f398b8ffd8be76b4d3b052dad768e5e8f3
Byron Reams <blreams@gmail.com>
2019-11-17 00:01:42 +0000

    modified pick_reminder.sh to use correct venv name

--- pick_reminder.sh
+++ pick_reminder.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 source virtualenvwrapper.sh
-workon cdcpool_py3
+workon cdcpool
 /home/collegefootballpick10/.virtualenvs/cdcpool_py3/bin/postactivate
-workon cdcpool_py3
+workon cdcpool
 python /home/collegefootballpick10/collegefootballpick10/email_players_with_no_picks.py
 
